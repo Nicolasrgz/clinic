2c36aef49d5e0e810f3d97a4d236c88c491a02cf
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-08-10 12:45:38 -0300

    insercion many to many

--- clinic/src/main/java/com/dev/clinic/ClinicApplication.java
+++ clinic/src/main/java/com/dev/clinic/ClinicApplication.java
@@ -21,21 +21,52 @@ public class ClinicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicApplication.class, args);
 	}
+
 	@Bean
 	public CommandLineRunner initData(PatientRepository patientRepository, ClinicHistoryRepository clinicHistoryRepository, MedicRepository medicRepository, MedicalSpecialtiesRepository medicalSpecialtiesRepository) {
 		return args -> {
-			//relation one to one patient wicht clinicHistory
+			//one to one
 			Patient patient = new Patient("nicolas", "gonzalez", 44994133, "klasdjds");
 			patientRepository.save(patient);
 			ClinicHistory clinicHistory = new ClinicHistory(patient.getLastName() + " " + patient.getName(), LocalDate.now(), "doctor pepe", "cancer","se puede curar");
 			patient.addClinicHistory(clinicHistory);
 			clinicHistoryRepository.save(clinicHistory);
 
+			//many to many
 			Medic drPepe = new Medic("pepe", "gomez", 40, "453-567-2");
+			Medic drJose = new Medic("jose", "perez", 40, "453-567-2");
+			Medic drJake = new Medic("jake", "gonzalez", 40, "453-567-2");
+			Medic drKeo = new Medic("keo", "kidd", 40, "453-567-2");
 			medicRepository.save(drPepe);
-			MedicalSpecialties cardiology = new MedicalSpecialties("cardiology", "A ");
+			medicRepository.save(drJose);
+			medicRepository.save(drJake);
+			medicRepository.save(drKeo);
+
+			MedicalSpecialties cardiology = new MedicalSpecialties("cardiology", "A");
+			MedicalSpecialties kinesiologist = new MedicalSpecialties("kinesiologist", "B");
 			drPepe.addMedicalSpecialty(cardiology);
+			drJose.addMedicalSpecialty(cardiology);
+			drJake.addMedicalSpecialty(cardiology);
+			drKeo.addMedicalSpecialty(cardiology);
+
+			drPepe.addMedicalSpecialty(kinesiologist);
+			drJose.addMedicalSpecialty(kinesiologist);
+
+			cardiology.addMedic(drPepe);
+			cardiology.addMedic(drJake);
+			cardiology.addMedic(drJose);
+			cardiology.addMedic(drKeo);
+
+			cardiology.addMedic(drPepe);
+			cardiology.addMedic(drJose);
+
 			medicalSpecialtiesRepository.save(cardiology);
+			medicalSpecialtiesRepository.save(kinesiologist);
+
+			medicRepository.save(drPepe);
+			medicRepository.save(drJose);
+			medicRepository.save(drJake);
+			medicRepository.save(drKeo);
 		};
 	}
 
