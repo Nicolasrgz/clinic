a14b955053ef56cb519ac22574ac70d51bbf72a9
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-08-14 17:40:31 -0300

    primeros pasos front

---
+++ clinic/src/main/resources/static/web/Vue/index.js
@@ -0,0 +1,21 @@
+const {createApp} = Vue;
+const app = createApp({
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+
+    },
+    created(){
+
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+})
+app.mount("#app")
