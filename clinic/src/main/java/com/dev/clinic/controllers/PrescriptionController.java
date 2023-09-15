1aab59b22c3c21c3a66abf5e3b6c60b25e122d2c
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-09-14 23:49:00 -0300

    prescripciones 1.5

--- clinic/src/main/java/com/dev/clinic/controllers/PrescriptionController.java
+++ clinic/src/main/java/com/dev/clinic/controllers/PrescriptionController.java
@@ -2,14 +2,21 @@ package com.dev.clinic.controllers;
 
 import com.dev.clinic.dtos.PrescriptionCreatedDTO;
 import com.dev.clinic.dtos.PrescriptionDTO;
+import com.dev.clinic.models.Medic;
+import com.dev.clinic.models.Patient;
+import com.dev.clinic.models.Prescription;
 import com.dev.clinic.repositories.ClinicHistoryRepository;
+import com.dev.clinic.repositories.MedicRepository;
+import com.dev.clinic.repositories.PatientRepository;
 import com.dev.clinic.repositories.PrescriptionRepository;
 import com.dev.clinic.services.service.PrescriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -22,15 +29,37 @@ public class PrescriptionController {
     private PrescriptionRepository prescriptionRepository;
     @Autowired
     private ClinicHistoryRepository clinicHistoryRepository;
+    @Autowired
+    private PatientRepository patientRepository;
+    @Autowired
+    private MedicRepository medicRepository;
 
     @GetMapping("/prescriptions")
     public List<PrescriptionDTO>getPrescriptions(){
         return prescriptionService.getPrescriptions();
     }
 
+    @Transactional
     @PostMapping("/prescription/created")
     public ResponseEntity<Object> createdPrescription(@Validated@RequestBody PrescriptionCreatedDTO prescriptionCreatedDTO){
 
+        if (prescriptionCreatedDTO.getDni() == null){
+            return new ResponseEntity<>("The DNI field is incomplete", HttpStatus.FORBIDDEN);
+        }
+        if (prescriptionCreatedDTO.getRegistrationNumber().isBlank()){
+            return new ResponseEntity<>("The registration number field is incomplete", HttpStatus.FORBIDDEN);
+        }
+
+        Patient patient = patientRepository.findByDni(prescriptionCreatedDTO.getDni());
+        Medic medic = medicRepository.findByRegistrationNumber(prescriptionCreatedDTO.getRegistrationNumber());
+
+        Prescription prescription = new Prescription(medic.getName() + " " + medic.getLastName(), patient.getName() + " " + patient.getLastName(), prescriptionCreatedDTO.getMedicationPrescript(), prescriptionCreatedDTO.getRegistrationNumber());
+        medic.addPrescription(prescription);
+        patient.getClinicHistory().addPrescription(prescription);
+
+        prescriptionRepository.save(prescription);
+        medicRepository.save(medic);
+        patientRepository.save(patient);
 
 
         return  ResponseEntity.ok().build();
